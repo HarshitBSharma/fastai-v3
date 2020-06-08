f1f984b29f29daf41e1eb301c0bde44f2ccdbc9f
sharmaharshit4262 <Nandini101>
2020-06-08 15:24:45 +0530

    Added Threshold

--- app/server.py
+++ app/server.py
@@ -80,7 +80,7 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
+    prediction = learn.predict(img, thresh=0.7)[0]
     return JSONResponse({'result': str(f"It's a {prediction} !")})
 
 
